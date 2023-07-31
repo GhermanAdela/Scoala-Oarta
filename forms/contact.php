51efa97c45cf9fe0249013c1233fbd826233e384
Adela <adela.gherman@softexco.ro>
2023-07-31 08:30:19 +0300

    contact

--- forms/contact.php
+++ forms/contact.php
@@ -7,8 +7,8 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
-
+  $receiving_email_address = 'ghermanadelaiulia@yahoo.com';
+die('test');
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
@@ -24,14 +24,14 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => 'ghermanadelaiulia26@yahoo.com',
+    'username' => 'test',
+    'password' => 'OartadeJos156*',
     'port' => '587'
   );
-  */
+
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
